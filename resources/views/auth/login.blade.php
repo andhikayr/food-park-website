56f611ba9436799289c8487217631e9b7e55057a
andhikayr <andhikayr9@gmail.com>
2023-12-08 09:15:25 +0700

    tambah proses login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,59 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('frontend.layouts.master')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('title')
+    Sign In
+@endsection
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@include('frontend.components.breadcrumb')
+
+@section('content')
+<section class="fp__signin" style="background: url(images/login_bg.jpg);">
+    <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+        <div class="container">
+            <div class="row wow fadeInUp" data-wow-duration="1s">
+                <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                    <div class="fp__login_area">
+                        <h2>selamat datang kembali!</h2>
+                        <p>sign in untuk melanjutkan</p>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput">
+                                        <label>email</label>
+                                        <input  type="email" name="email" value="{{ old('email') }}" required autofocus placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput">
+                                        <label>password</label>
+                                        <input  type="password" name="password" value="{{ old('password') }}" required placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput fp__login_check_area">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" id="remember_me" name="remember">
+                                            <label class="form-check-label" for="remember_me">
+                                                Ingat Saya
+                                            </label>
+                                        </div>
+                                        <a href="{{ route('password.request') }}">Lupa Password ?</a>
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput">
+                                        <button type="submit" class="common_btn">login</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <p class="create_account">Belum punya akun ? <a href="{{ route('register') }}">Buat Akun</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</section>
+@endsection
---
+++ resources/views/frontend/components/breadcrumb.blade.php
@@ -0,0 +1,19 @@
+<!--=============================
+        BREADCRUMB START
+    ==============================-->
+<section class="fp__breadcrumb" style="background: url({{ asset('frontend/images/counter_bg.jpg') }});">
+    <div class="fp__breadcrumb_overlay">
+        <div class="container">
+            <div class="fp__breadcrumb_text">
+                <h1>@yield('title')</h1>
+                <ul>
+                    <li><a href="{{ url('/') }}">home</a></li>
+                    <li class="text-white">@yield('title')</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!--=============================
+        BREADCRUMB END
+    ==============================-->
