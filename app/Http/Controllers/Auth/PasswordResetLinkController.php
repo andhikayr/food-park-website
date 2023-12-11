031b5d24e0ad72210498a47ff740f9a84f4f4d78
andhikayr <andhikayr9@gmail.com>
2023-12-11 09:16:13 +0700

    instalasi sweetalert2 laravel + register + reset password

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class PasswordResetLinkController extends Controller
 {
@@ -36,6 +37,7 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        Alert::success('Sukses', 'Link reset password anda telah berhasil dikirim');
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -36,4 +36,59 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('frontend.layouts.master')
+
+@section('title')
+    Reset Password
+@endsection
+
+@include('frontend.components.breadcrumb')
+
+@section('content')
+<section class="fp__signup" style="background: url({{ asset('frontend/images/login_bg.jpg') }});">
+    <div class="fp__signup_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+        <div class=" container">
+            <div class="row wow fadeInUp" data-wow-duration="1s">
+                <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                    <div class="fp__login_area">
+                        <h2>Reset password</h2>
+                        <p>Isi form dibawah ini untuk mereset password anda</p>
+                        <form method="POST" action="{{ route('password.store') }}">
+                            @csrf
+                            <!-- Password Reset Token -->
+                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput">
+                                        <label>email</label>
+                                        <input type="email" placeholder="Email" name="email" value="{{ old('email', $request->email) }}" readonly>
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput">
+                                        <label>password</label>
+                                        <input type="password" placeholder="Password" name="password" required>
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput">
+                                        <label>konfirmasi password</label>
+                                        <input type="password" placeholder="Konfirmasi Password" name="password_confirmation">
+                                    </div>
+                                </div>
+                                <div class="col-xl-12">
+                                    <div class="fp__login_imput">
+                                        <button type="submit" class="common_btn">Reset Password</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
