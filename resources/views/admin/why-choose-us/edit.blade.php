bba54eb149dd89ea9fdb262e9bfb695898992a85
andhikayr <andhikayr9@gmail.com>
2024-01-10 09:49:44 +0700

    edit ukuran kolom sama seperti form create "Mengapa Memilih Kita"

--- resources/views/admin/why-choose-us/edit.blade.php
+++ resources/views/admin/why-choose-us/edit.blade.php
@@ -15,7 +15,7 @@
                         <form method="POST" class="row g-3" action="{{ route('admin.why-choose-us.update', $WhyChooseUs->id) }}">
                             @csrf
                             @method('PUT')
-                            <div class="col-12">
+                            <div class="col-md-6">
                                 <label for="GetIconPicker" class="form-label">Ikon</label>
                                 <br>
                                 <div class="ms-3" data-toggle="tooltip" title="" data-original-title="Preview of selected Icon">
@@ -25,16 +25,16 @@
                                     data-iconpicker-preview="i#GetIconPicker" name="icon" class="btn btn-primary mt-3">Pilih Ikon</button>
                                 <input type="hidden" id="GetIconPicker" name="icon" required placeholder="Hidden etc. input for icon classname" autocomplete="off" spellcheck="false" value="{{ $WhyChooseUs->icon }}">
                             </div>
-                            <div class="col-12">
+                            <div class="col-md-6">
                                 <label for="title" class="form-label">Judul *</label>
                                 <input type="text" class="form-control" id="title" name="title" required
                                     maxlength="30" value="{{ $WhyChooseUs->title }}">
                             </div>
-                            <div class="col-12">
+                            <div class="col-md-6">
                                 <label for="short_description" class="form-label">Deskripsi Singkat *</label>
                                 <input type="text" class="form-control" id="short_description" name="short_description" maxlength="100" value="{{ $WhyChooseUs->short_description }}">
                             </div>
-                            <div class="col-12">
+                            <div class="col-md-6">
                                 <label for="status" class="form-label">Status</label>
                                 <select name="status" id="status" class="form-select">
                                     <option value="1" @selected($WhyChooseUs->status === 1)>Ya</option>
