5f2de42df40cd0d009ec78f9e5af4114ee0acb83
andhikayr <andhikayr9@gmail.com>
2024-02-16 08:34:42 +0700

    fix cache tidak terhapus setelah update data pengaturan umum

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Setting;
+use App\Services\SettingsService;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -33,6 +34,9 @@ class SettingController extends Controller
                 ]
             );
         }
+
+        $settingsService = app(SettingsService::class);
+        $settingsService->clearCacheSettings();
 
         Alert::success('Sukses', 'Data berhasil diubah');
         return back();
