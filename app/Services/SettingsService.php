42daf89ac817df5ea47e96840586da4e8c974455
andhikayr <andhikayr9@gmail.com>
2024-02-16 08:19:44 +0700

    set SettingServiceProvider secara global

---
+++ app/Services/SettingsService.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Services;
+
+use App\Models\Setting;
+use Cache;
+
+class SettingsService {
+    public function getSettings() {
+        return Cache::rememberForever('settings', function () {
+            return Setting::pluck('value', 'key')->toArray();
+        });
+    }
+
+    public function setGlobalSettings() : void {
+        $settings = $this->getSettings();
+        config()->set('settings', $settings);
+    }
+
+    public function clearCacheSettings() : void {
+        Cache::forget('settings');
+    }
+}
