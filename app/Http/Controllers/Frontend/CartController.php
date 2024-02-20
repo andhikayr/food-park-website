26262ad76e51c1944cef7ab1bbb50f4902666c5d
andhikayr <andhikayr9@gmail.com>
2024-02-20 20:13:55 +0700

    tampilkan pesan sukses dan error pada frontend tambah produk ke keranjang

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -9,45 +9,50 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function addToCart(Request $request) {
-        $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
-        $productSize = $product->productSizes->where('id', $request->product_size)->first();
-        $productOptions = $product->productOptions->whereIn('id', $request->product_option);
+    public function addToCart(Request $request)
+    {
+        try {
+            $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
+            $productSize = $product->productSizes->where('id', $request->product_size)->first();
+            $productOptions = $product->productOptions->whereIn('id', $request->product_option);
 
-        $options = [
-            'product_size' => [],
-            'product_options' => [],
-            'product_info' => [
-                'image' => $product->thumb_image,
-                'slug' => $product->slug
-            ]
-        ];
-
-        if($productSize !== null) {
-            $options['product_size'][] = [
-                'id' => $productSize?->id,
-                'name' => $productSize?->name,
-                'price' => $productSize?->price
+            $options = [
+                'product_size' => [],
+                'product_options' => [],
+                'product_info' => [
+                    'image' => $product->thumb_image,
+                    'slug' => $product->slug
+                ]
             ];
+
+            if ($productSize !== null) {
+                $options['product_size'][] = [
+                    'id' => $productSize?->id,
+                    'name' => $productSize?->name,
+                    'price' => $productSize?->price
+                ];
+            }
+
+            foreach ($productOptions as $option) {
+                $options['product_options'][] = [
+                    'id' => $option->id,
+                    'name' => $option->name,
+                    'price' => $option->price
+                ];
+            }
+
+            Cart::add([
+                'id' => $product->id,
+                'name' => $product->name,
+                'qty' => $request->quantity,
+                'price' => $product->offer_price > 0 ? $product->offer_price : $product->price,
+                'weight' => 0,
+                'options' => $options
+            ]);
+
+            return response(['status' => 'success', 'message' => 'Produk berhasil ditambahkan ke keranjang'], 200);
+        } catch (\Exception $e) {
+            return response(['status' => 'error', 'message' => 'Ada sesuatu yang salah'], 500);
         }
-
-        foreach ($productOptions as $option) {
-            $options['product_options'][] = [
-                'id' => $option->id,
-                'name' => $option->name,
-                'price' => $option->price
-            ];
-        }
-
-        Cart::add([
-            'id' => $product->id,
-            'name' => $product->name,
-            'qty' => $request->quantity,
-            'price' => $product->offer_price > 0 ? $product->offer_price : $product->price,
-            'weight' => 0,
-            'options' => $options
-        ]);
-
-        return response(['status' => 'success', 'message' => 'Produk ditambahkan ke keranjang'], 200);
     }
 }
