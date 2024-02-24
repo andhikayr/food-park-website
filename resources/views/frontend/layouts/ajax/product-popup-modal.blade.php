4b1c9af7337716aed9371a0a41fd96a4b10db9bd
andhikayr <andhikayr9@gmail.com>
2024-02-24 10:01:27 +0700

    hapus fungsi formatRupiah karena sudah ada di global_helper.php

--- resources/views/frontend/layouts/ajax/product-popup-modal.blade.php
+++ resources/views/frontend/layouts/ajax/product-popup-modal.blade.php
@@ -136,23 +136,6 @@
             $('#total_price').text("Rp. " + totalPrice);
         }
 
-        // format rupiah
-        function formatRupiah(angka, prefix) {
-            var number_string = angka.replace(/[^,\d]/g, '').toString(),
-                split = number_string.split(','),
-                sisa = split[0].length % 3,
-                rupiah = split[0].substr(0, sisa),
-                ribuan = split[0].substr(sisa).match(/\d{3}/gi);
-
-            if (ribuan) {
-                separator = sisa ? '.' : '';
-                rupiah += separator + ribuan.join('.');
-            }
-
-            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-            return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-        }
-
         // function tambah ke keranjang
         $("#modal_add_to_cart_form").on('submit', function (e) {
             e.preventDefault();
